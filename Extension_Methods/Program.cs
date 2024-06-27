1dd192fa617a4d5135888eee2f3e566ee28b9874
3472592 <3472592@gmail.com>
2024-06-27 05:57:59 -0400

    Add files via upload

---
+++ Extension_Methods/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace Extension_Methods;
+
+public class Program 
+{
+    private static void Main(string[] args)
+     {
+        if (args is null)
+        {
+            throw new ArgumentNullException(nameof(args));
+        }
+
+        OrdinaryHuman steve = new(185);
+        Console.WriteLine(steve.BreakWalls(89.2));
+    }
+
+}
+public sealed class OrdinaryHuman
+{
+    private readonly int age = 18;
+    private readonly int weight = 55;
+
+    public OrdinaryHuman (int weight)
+    {
+        this.weight = weight;
+    }
+    public void GoToWork()
+    {
+        Console.WriteLine(" Going to work...");
+    }
+
+    public void PayBills()
+    {
+        Console.WriteLine("Paying bills...");
+    }
+
+}
+
+public static class SuperSoldierSerum
+{
+    public static string BreakWalls(this OrdinaryHuman h, double wallDensity)
+    {
+        if (h is null)
+        {
+            throw new ArgumentNullException(nameof(h));
+        }
+
+        return ("I broke through a wall of " + wallDensity + " density.");
+    }
+}
